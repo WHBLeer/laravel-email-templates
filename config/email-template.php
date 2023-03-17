deb8edf566691026652cb607be792dcbd96f2fe5
王宏彬 <wanghongbin816@gmail.com>
2023-03-17 18:05:17 +0800

    fixbug

--- config/email-template.php
+++ config/email-template.php
@@ -1,17 +1,17 @@
 <?php
 
 return [
-    'top_logo'          => 'https://www.simple-pleb.com/storage/uploads/logo/logo_big.svg',
-    'welcome_url'       => 'https://www.simple-pleb.com/login',
-    'street_address'    => '',
-    'phone_number'      => '(738) 479-6719',
-    'info_email'        => 'info@website.com',
-    'play_url'          => '',
-    'ios_url'           => '',
-
-    // To remove from the email footer - make the value blank
-    'facebook_url'      => 'https://www.facebook.com',
-    'twitter_url'       => 'https://www.twitter.com',
-    'instagram_url'     => 'https://www.instagram.com',
-    'pinterest_url'     => 'https://www.pinterest.com',
+	'home_url'          => '/',
+	'logo_url'          => '/uploads/logo.png',
+	'login_url'         => '/login',
+	'enquiry_url'       => '/enquiry',
+	'street_address'    => '深圳市南山区粤海街道华大科技大厦北座502',
+	'phone_number'      => '(+86) 7890-119',
+	'info_email'        => 'info@uhaveshop.com',
+	
+	// To remove from the email footer - make the value blank
+	'facebook_url'      => 'https://www.facebook.com',
+	'twitter_url'       => 'https://www.twitter.com',
+	'instagram_url'     => 'https://www.instagram.com',
+	'pinterest_url'     => 'https://www.pinterest.com',
 ];
--- resources/views/email/main/header.blade.php
+++ resources/views/email/main/header.blade.php
@@ -13,7 +13,7 @@
 			<table border="0" align="left" cellpadding="0" cellspacing="0" role="presentation" class="row" style="max-width:280px;">
 				<tr>
 					<td align="left" class="center-text">
-						<a href="{{ url('/') }}"><img style="max-height:72px;border:0px;display: inline!important;" src="{{ asset('uploads/logo.png') }}" border="0" alt="logo"></a>
+						<a href="{{ url(config('email-template.home_url') }}"><img style="max-height:72px;border:0px;display: inline!important;" src="{{ asset(config('email-template.logo_url')) }}" border="0" alt="logo"></a>
 					</td>
 				</tr>
 			</table>
@@ -32,10 +32,10 @@
 			<table border="0" align="right" cellpadding="0" cellspacing="0" role="presentation" class="row" width="280" style="max-width:280px;">
 				<tr>
 					<td align="right" class="center-text" style="font-size: 18px;line-height: 20px;color: #6e6e6e;">
-						<a href="{{ url('/enquiry') }}">{{__('Enquiry')}}</a>
+						<a href="{{ url(config('email-template.enquiry_url')) }}">{{__('Enquiry')}}</a>
 					</td>
 					<td align="right" class="center-text" style="font-size: 18px;line-height: 20px;color: #6e6e6e;">
-						<a href="{{ url('/login') }}">{{__('Login')}}</a>
+						<a href="{{ url(config('email-template.login_url')) }}">{{__('Login')}}</a>
 					</td>
 				</tr>
 			</table>
